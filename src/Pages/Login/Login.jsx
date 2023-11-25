0a6828223321ac38341c3dc76a19158d55783a8d
Rasibul <rasibul179@gmail.com>
2023-11-25 12:33:44 +0600

    create register page and login page by use react hook from

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,49 @@
-
+import { Link } from "react-router-dom";
+import { Helmet } from 'react-helmet-async';
 
 const Login = () => {
+    const handelLogin = e => {
+        e.preventDefault()
+        const from = e.target
+        const email = from.email.value
+        const password = from.password.value
+        console.log(email, password)
+    }
     return (
-        <div>
-            
+        <div className="hero  ">
+            <Helmet>
+                <title>Medical Camp | Login</title>
+            </Helmet>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="">
+                    <img src="https://i.ibb.co/2ccw40X/authentication.gif" alt="" />
+                </div>
+                <div className="card w-full max-w-sm ">
+                    <h2 className='text-4xl text-center font-bold'>Please Login </h2>
+                    <form onSubmit={handelLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                        <label className="label font-bold">
+                            New here? <Link to="/register" className="label-text-alt link link-hover font-bold">Create an account</Link>
+                        </label>
+                        {/* <SocialLogin></SocialLogin> */}
+                    </form>
+                </div>
+
+            </div>
         </div>
     );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,9 +1,63 @@
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { Helmet } from 'react-helmet-async';
 
 
 const Register = () => {
+    const { register, handleSubmit, reset, formState: { errors }, } = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+    }
     return (
-        <div>
-            
+        <div className="hero  ">
+            <Helmet>
+                <title>Medical Camp | Register</title>
+            </Helmet>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="">
+                    <img src="https://i.ibb.co/2ccw40X/authentication.gif" alt="" />
+                </div>
+                <div className="card w-full max-w-sm ">
+                    <h2 className='text-4xl text-center font-bold'>Please Sign Up </h2>
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="name" placeholder="name" name="name" {...register("name")} required className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" name="email" {...register("email")} className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo</span>
+                            </label>
+                            <input type="text" placeholder="Photo Url" name="photo" {...register("photoURL")} className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" {...register("password", {
+                                pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{6}/
+                            })} name="password" className="input input-bordered" required />
+                            {errors.password?.type === 'pattern' && <p className='text-red-500'>password must have one number,one uppercase,onelowercase, special character and than 6 characters</p>}
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Register" />
+                        </div>
+                        <h2 className="text-sm font-bold">
+                            Alredy Have An Account? <Link to="/login" className="label-text-alt link link-hover text-sm font-bold">Please Login</Link>
+                        </h2>
+                        {/* <SocialLogin></SocialLogin> */}
+                    </form>
+                </div>
+
+            </div>
         </div>
     );
 };
