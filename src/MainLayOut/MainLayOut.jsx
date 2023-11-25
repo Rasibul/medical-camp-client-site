e97c3ac0cf2aca0302476bf150f332ed1c5e3ce3
Rasibul <rasibul179@gmail.com>
2023-11-25 10:29:54 +0600

    create a navbar and connect to the route and show nav section in ui

--- src/MainLayOut/MainLayOut.jsx
+++ src/MainLayOut/MainLayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Home/Header/NavBar/NavBar";
 
 
 const MainLayOut = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
